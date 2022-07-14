bfa5625a595c185e1b5ce7813d28093014397ccc
tialaron <30555530+tialaron@users.noreply.github.com>
2022-07-14 12:22:45 +0300

    Update laboratory4.py

--- laboratory4.py
+++ laboratory4.py
@@ -120,20 +120,9 @@ with col3:
                           image11 = Image.open(file_path)
                           st.image(file_path) 
                           img11 = image11.resize((28, 28), Image.LANCZOS)   
-                          img11.save(file_path) 
-                          
-                          #img12 = img11.convert("L")
-                          #imgData = np.asarray(img12)
-                          #step_lobe = .4
-                          #mid_img_color = np.sum(imgData) / imgData.size
-                          #min_img_color = imgData.min()
-                          
-                          
-                          #THRESHOLD_VALUE = int(mid_img_color - (mid_img_color - min_img_color) * step_lobe)
-                          #thresholdedData = (imgData < THRESHOLD_VALUE) * 1.0
+                          img11.save(file_path)                        
                           imgData1 = np.expand_dims(np.asarray(img11.convert("L")), axis=0)
 
-                        
 with col4:
               st.write('Она распознала это как...')
               if isbutton1:
@@ -141,49 +130,28 @@ with col4:
                           y_maxarg = np.argmax(y_predict1, axis=1)
                           st.subheader(int(y_maxarg))
 
-with st.expander('Пункт 7.'):
-    st.write('Включи коррекцию яркости. Посмотри, улучшило ли это изображение негатива цифры.'
-             ' Зарисуй результат, как указано выше.')
-    col5,col6 = st.columns(2)
-    with col5:
-              value_sli = st.slider('Коррекция яркости', 0.0, 100.0, 50.0)
-    with col6:
-              st.write('Яркость',value_sli)
-              image111 = Image.open(file_path)
-              enhancer = ImageEnhance.Brightness(image111)
-            
-              factor = 2*value_sli / 100 #фактор изменения
-                
-              im_output = enhancer.enhance(factor)
-              im_output.save(file_path)
-              #img111 = image111.resize((28, 28), Image.LANCZOS)     
-              #img121 = img111.convert("L")
-              #imgData = np.asarray(img121)
-              #step_lobe = value_sli / 100
-              #mid_img_color = np.sum(imgData) / imgData.size
-              #min_img_color = imgData.min()
-              #THRESHOLD_VALUE = (mid_img_color - (mid_img_color - min_img_color) * step_lobe)
-              #thresholdedData = (imgData < THRESHOLD_VALUE) * 1.0
-              #imgData1 = np.expand_dims(thresholdedData, axis=0)
-              #st.write(imgData1.shape)
-              #tgt1 = np.squeeze(imgData1)
-              #st.write(tgt1.shape)
-              #im111 = Image.fromarray(tgt1)
-              #st.write(imgData1)
-              #im111.save(file_path)
-              st.image(file_path)
-              #y_predict1 = model_2d.predict(imgData1) 
-              #y_maxarg = np.argmax(y_predict1, axis=1)
-              #st.subheader(int(y_maxarg))
+st.write('Пункт 5.')
+st.write('Включите коррекцию яркости. Посмотрите, улучшило ли это изображение негатива цифры.'
+         ' Зарисуйте результат, как указано выше.')
+col5,col6 = st.columns(2)
+with col5:
+         value_sli = st.slider('Коррекция яркости', 0.0, 100.0, 50.0)
+with col6:
+         st.write('Яркость',value_sli)
+         image111 = Image.open(file_path)
+         enhancer = ImageEnhance.Brightness(image111)
+         factor = 2*value_sli / 100 #фактор изменения
+         im_output = enhancer.enhance(factor)
+         im_output.save(file_path)
 
-with st.expander('Пункт 8.'):
-    st.write('Нажми на кнопку распознавания, запиши результат.')
-    isbutton2 = st.button('Распознать еще картнку')
-    col7,col8 = st.columns(2)
-    with col7:
+st.write('Пункт 6.')
+st.write('Нажмите на кнопку распознавания, запишите результат.')
+isbutton2 = st.button('Распознать еще картнку')
+col7,col8 = st.columns(2)
+with col7:
              if isbutton2:
                    st.image(file_path)
-    with col8:
+with col8:
              if isbutton2:
                    image112 = Image.open(file_path)
                    img111 = image112.resize((28, 28), Image.LANCZOS)  
@@ -197,29 +165,28 @@ with st.expander('Пункт 8.'):
                    imgData1 = np.expand_dims(thresholdedData, axis=0)
                    y_predict1 = model_2d.predict(imgData1)
                    y_maxarg = np.argmax(y_predict1, axis=1)
-                   st.subheader(int(y_maxarg))
-                   
-              
+                   st.subheader(int(y_maxarg))  
 
-with st.expander('Пункт 9.'):
-    st.write('Включи фильтр Гаусса, если такая кнопка есть, нажми на кнопку распознавания, запиши результат.')
-    col9,col10 = st.columns(2)
-    with col9:
+st.write('Пункт 7.')
+st.write('Скорректируйте изображение с помощью фильтра Гаусса. Нажмите на кнопку распознавания, запишите результат.')
+col9,col10 = st.columns(2)
+with col9:
             value_gaus = st.slider('Фильтр Гаусса', 0, 10, 0)
-    with col10:
+with col10:
             st.write('Фильтр Гаусса',value_gaus)
             image222 = Image.open(file_path)
             im2 = image222.filter(ImageFilter.GaussianBlur(radius = value_gaus))
             im2.save(file_path)
             st.image(file_path)
-with st.expander('Пункт 10'):
-    st.write('Попробуем теперь еще раз распознать картинку.')
-    isbutton3 = st.button('Распознать картнку еще раз')
-    col11,col12 = st.columns(2)
-    with col11:
+            
+st.write('Пункт 8.')
+st.write('Попробуем теперь еще раз распознать картинку.')
+isbutton3 = st.button('Распознать картнку еще раз')
+col11,col12 = st.columns(2)
+with col11:
             if isbutton3:
                    st.image(file_path)
-    with col12:
+with col12:
             if isbutton3:
                    image333 = Image.open(file_path)
                    img333 = image333.resize((28, 28), Image.LANCZOS) 
@@ -235,33 +202,28 @@ with st.expander('Пункт 10'):
                    y_maxarg2 = np.argmax(y_predict2, axis=1)
                    st.subheader(int(y_maxarg2)) 
                     
-with st.expander('Пункт 11.'):
-    st.write('Сделай выводы, какие именно фильтры и как влияют на результат эксперимента')
-with st.expander('Пункт 12.'):
-    st.write('Посмотрим как "видит" картинку нейронная сеть')
-    col13,col14 = st.columns(2)
-    with col13:
-            value_thres = st.slider('Порог отсечки', 0, 100, 50)
-    with col14:
-            st.write('Порог отсечки',value_thres)
-            image444 = Image.open(file_path)
-            #i1 = image444.resize((28, 28), Image.LANCZOS) 
-            i2 = image444.convert("L")
-            i3 = np.asarray(i2)
-            step_lobe = value_thres / 100
-            mid_img_color = np.sum(i3) / i3.size
-            min_img_color = i3.min()
-            THRESHOLD_VALUE = (mid_img_color - (mid_img_color - min_img_color) * step_lobe)
-            thresholdedData = (i3 < THRESHOLD_VALUE) * 255.0
-            imm1 = Image.fromarray(thresholdedData)
-            imm1 = imm1.convert("L")
-            imm1.save(file_path)
-            st.write(imm1) 
-            st.image(file_path)
-            #i4 = np.expand_dims(thresholdedData, axis=0)
-            #imm1 = Image.fromarray(i4) 
-            #imm1.save(file_path)
-            #st.image(file_path)
+st.write('Пункт 9.')
+st.write('Сделай выводы, какие именно фильтры и как влияют на результат эксперимента')
+st.write('Пункт 10.')
+st.write('Посмотрим как "видит" картинку нейронная сеть')
+col13,col14 = st.columns(2)
+with col13:
+         value_thres = st.slider('Порог отсечки', 0, 100, 50)
+with col14:
+         st.write('Порог отсечки',value_thres)
+         image444 = Image.open(file_path)
+         i2 = image444.convert("L")
+         i3 = np.asarray(i2)
+         step_lobe = value_thres / 100
+         mid_img_color = np.sum(i3) / i3.size
+         min_img_color = i3.min()
+         THRESHOLD_VALUE = (mid_img_color - (mid_img_color - min_img_color) * step_lobe)
+         thresholdedData = (i3 < THRESHOLD_VALUE) * 255.0
+         imm1 = Image.fromarray(thresholdedData)
+         imm1 = imm1.convert("L")
+         imm1.save(file_path)
+         st.write(imm1) 
+         st.image(file_path)
             
-with st.expander('Пожелания и замечания'):                
-    st.write('https://docs.google.com/spreadsheets/d/1TuGgZsT2cwAIlNr80LdVn4UFPHyEePEiBE-JG6IQUT0/edit?usp=sharing')
+st.write('Пожелания и замечания')                
+st.write('https://docs.google.com/spreadsheets/d/1TuGgZsT2cwAIlNr80LdVn4UFPHyEePEiBE-JG6IQUT0/edit?usp=sharing')
